547e84feb60e40fd0ffd15fc4f747d78d8cb8615
Joao Antonio <joao.antonio05@outlook.com>
2026-06-15 14:29:35 -0300

    todo: Começar a fazer a raiz das mensagens

--- components.c
+++ components.c
@@ -1,7 +1,7 @@
 #include "clay.h"
 #include "headers/persona_theme.h"
 
-void MessageBoxComponent (Clay_String messageText) {
+void InstantMessageBoxComponent (Clay_String messageText) {
     static CustomLayoutElement messageOuterBox = {
         .type = CUSTOM_LAYOUT_ELEMENT_TYPE_POLYGON,
         .customData.polygon = {
@@ -23,28 +23,65 @@ void MessageBoxComponent (Clay_String messageText) {
         }
     };
 
-    CLAY_AUTO_ID({
-        .custom = { .customData = &messageOuterBox},
-        .backgroundColor = P5_COLOR_WHITE,
-        .layout = {
-            .sizing = {CLAY_SIZING_FIXED(300), CLAY_SIZING_FIXED(80)},
-            .padding = {4, 4, 4, 4},
-        },
+    static CustomLayoutElement messageStemWhiteLeft = {
+        .type = CUSTOM_LAYOUT_ELEMENT_TYPE_POLYGON,
+        .customData.polygon = {
+            .color = P5_COLOR_WHITE,
+            .topLeft = {0, 0},
+            .topRight = {0, 0},
+            .bottomLeft = {0, 0},
+            .bottomRight = {0, 0}
+        }
+    };
 
+    static CustomLayoutElement messageStemWhiteRight = {
+        .type = CUSTOM_LAYOUT_ELEMENT_TYPE_POLYGON,
+        .customData.polygon = {
+            .color = P5_COLOR_WHITE,
+            .topLeft = {0, 0},
+            .topRight = {15, -12},
+            .bottomLeft = {-20, 0},
+            .bottomRight = {0, 10}
+        }
+    };
+
+    CLAY_AUTO_ID({
+        .layout = {
+            .layoutDirection = CLAY_LEFT_TO_RIGHT,
+        }   
     }){
         CLAY_AUTO_ID({
-            .custom = { .customData = &messageInnerBox},
-            .backgroundColor = P5_COLOR_BLACK,
+            //.border = {.width = {2, 2, 2, 2, 0}, .color = {0, 0, 255, 255},},
+            .custom = { .customData = &messageStemWhiteLeft},
             .layout = {
-                .sizing = {CLAY_SIZING_FIXED(290), CLAY_SIZING_FIXED(70)},
-                .padding = {16, 16, 16, 16},
+                .sizing = {CLAY_SIZING_FIXED(20), CLAY_SIZING_FIXED(15),},
             }
+        }) {
+
+        } 
+        CLAY_AUTO_ID({
+            //.border = {.width = {2, 2, 2, 2, 0}, .color = {0, 0, 255, 255},},
+            .custom = { .customData = &messageOuterBox},
+            .layout = {
+                .sizing = {CLAY_SIZING_FIXED(300), CLAY_SIZING_FIXED(80)},
+                .padding = {4, 4, 4, 4},
+            },
         }){
-            CLAY_TEXT(messageText, {
-                .fontSize = 16,
-                .textColor = P5_COLOR_WHITE,
-                .fontId = FONT_ID_BODY_MESSAGETEXT,
-            });
+            CLAY_AUTO_ID({
+                //.border = {.width = {2, 2, 2, 2, 0},.color = {0, 255, 0, 255},},
+                .custom = { .customData = &messageInnerBox},
+                .layout = {
+                    .sizing = {CLAY_SIZING_FIXED(290), CLAY_SIZING_FIXED(70)},
+                    .padding = {16, 16, 16, 16},
+                }
+            }){
+                CLAY_TEXT(messageText, {
+                    .fontSize = 16,
+                    .textColor = P5_COLOR_WHITE,
+                    .fontId = FONT_ID_BODY_MESSAGETEXT,
+                });
+            }
         }
     }
+
 }
